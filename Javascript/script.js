3e2f9d091427e70ed6cc28c37c12ee7744626ec3
Maruf Khan <marufatrai12@gmail.com>
2023-11-15 10:55:05 +0600

    JS-Class-7

---
+++ Javascript/script.js
@@ -0,0 +1,25 @@
+
+
+
+
+    // JavaScript Library Function for String.
+
+
+    //   Basic JavaScript Assignment. 
+
+    var firstName = prompt("Enter Your First Name : ");
+
+
+    var lastName = prompt("Enter Your Last Name : ");
+
+
+    var textAdd = firstName.concat(lastName);
+
+    var textUppercase = textAdd.toUpperCase();
+
+
+   //    var textL = textUppercase.length;
+
+   var uNamePosition = textUppercase.charAt(0);
+
+    document.write(uNamePosition);
